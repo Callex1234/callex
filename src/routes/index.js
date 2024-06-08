27cab2a53846a502814fe2c0a49ac9405a117bf7
yashmistry4u <nameisyash1@gmail.com>
2024-06-08 15:27:49 +0530

    middleware redirect route change

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,16 @@ const { handleWelcome } = require("../controller");
 const authRoutes = require("./adminAuth.routes");
 const { Admin } = require("mongodb");
 const adminRoutes = require("./admin.routes");
+const { login } = require("../controller/auth.controller");
 
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-  res.status(200).send(` ******  WELCOME to CALLEX  *******`);
+  return res.redirect("/login");
 });
 
 routes.get("/test", handleWelcome);
+routes.get("/login", login);
 
 routes.use("/admin", authRoutes);
 routes.use("/admin", adminRoutes);
